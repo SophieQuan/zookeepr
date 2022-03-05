ffb99bf369caea8485ec2b46573fc8dc6854eccb
SophieQuan <sophiequan7@gmail.com>
2022-03-04 21:25:36 -0500

    solved issue 3

--- server.js
+++ server.js
@@ -115,7 +115,6 @@ function validateAnimal(animal) {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
-
 app.get('/animals', (req, res) => {
     res.sendFile(path.join(__dirname, './public/animals.html'));
 });
