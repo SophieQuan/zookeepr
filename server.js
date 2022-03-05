f5d5f0c804dfc41076bd6de2baa6af1c15ce7446
SophieQuan <sophiequan7@gmail.com>
2022-03-04 21:16:19 -0500

    Implement a front end

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 const { animals } = require('./data/animals');
 
@@ -111,6 +112,19 @@ function validateAnimal(animal) {
     }
     return true;
 }
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`)
 })
