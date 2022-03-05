306052e73a9ce44075c4103509557176a96b06c5
SophieQuan <sophiequan7@gmail.com>
2022-03-04 21:22:04 -0500

    Implement a front end

--- server.js
+++ server.js
@@ -115,12 +115,15 @@ function validateAnimal(animal) {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
 app.get('/animals', (req, res) => {
     res.sendFile(path.join(__dirname, './public/animals.html'));
 });
+
 app.get('/zookeepers', (req, res) => {
     res.sendFile(path.join(__dirname, './public/zookeepers.html'));
 });
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
